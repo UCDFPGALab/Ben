4a8d75e26fdcd4ffcbf803c0a9014b851ae9218f
borpo <borpo@ucdavis.edu>
2014-04-03 16:32:03 -0700

    New loops implemented, goes back to the beginning ready for data state at the end of the test

--- RedCat_Main/RedCat/RedCatNewWithPortsII/RedCatNewWithPortsII.ino
+++ RedCat_Main/RedCat/RedCatNewWithPortsII/RedCatNewWithPortsII.ino
@@ -25,15 +25,11 @@ const char SET_CODES[] = {'a', 'b', 'c', 'd'};
 // Data and address integers
 int correctData = 170;
 int dataInt;
-int addressSteps = 0;
-long addressInt = 0;
 
 double holdAmount = 0.1;
-bool writing = true;
 
 int divider = 0;
-int numRuns = 1;           // Number of runs 
-int currentRun = 0;
+int numRuns = 1;           // Number of runs
 
 int delayAfterWrite = 10;  // Delay after writing (in milliseconds)
 int readsAfterFailure = 5; // Number of reads if there is a bad read
@@ -44,7 +40,6 @@ void setup()
 {
   Serial.begin(115200);
   establishContact();
-  int mod = 0;
   // Set modes of IO
   pinMode(CS, OUTPUT);
   pinMode(OE,OUTPUT);
@@ -64,128 +59,105 @@ void setup()
       i = 44;
     }
   }
-  
-  // Initialize serial connection
-  getData();
 
-  // Using PD0 through PD9 without PD4 and PD5. Thus, need to split the data at PD4 and PD5; 
-  mod = correctData & 15; // Get the last 4 digits
-  correctData >>= 4;  // Bit shift up to where 0's should start
-  correctData <<= 6;  // Add the two extra zeros
-  correctData += mod; // Re-add mod
-  //startTime  = micros(); //is this line necessary?
+  //startTime  = micros();
 }
 
 
 
-void loop()
-{
-  divider = (divider + 1) % 25000;
-  if(divider > 12500) {
-    digitalWrite(LED, HIGH);
-  }
-  else {
-    digitalWrite(LED, LOW);
-  }
-  
-  if(currentRun < numRuns) { // Check to make sure the correct number of runs are being done
-    if(addressSteps < NUM_ADDRESSES) { // Loop through all addresses then stop
-      if(writing) {  // Write and then read
-      
-        if(addressSteps == 0) { //starting write cycle
-          //REG_PIOD_OWER = 0x3CF;
-          // Set pins to outputs
-          pinMode(11, OUTPUT);
-          pinMode(12, OUTPUT);
-          for(i = 25; i < 31; i++) {
-            pinMode(i, OUTPUT);
-          }
-        }
-        writeData(addressInt, correctData);
-       
-        // Strange addition is necessary to ensure that dedicated pins do not get set. In this case, pins 33 through 41 are being used and then pins 45 through 51. Thus, when the following situation is met:
-        // 0000000 0000 111111111 0 -> 0000001 0000 000000000 0 
-        if ((addressInt & ADDRESS_MASK) < (ADDRESS_MASK - 1)) {
-           addressInt = addressInt + 2;
-        }
-        else {
-          addressInt = addressInt + 3074;
-        }
+void loop() {
+  getData();
+
+  for(int run = 0; run < numRuns; run++) { //main loop for the tests    
+    int addr = 0;
+    long addressInt = 0;
+    
+    // Current behavior is read all addresses, then write all the addresses.
+    // However, this can be changed to write and address, then read it right away, and so on.
+    
+    // REG_PIOD_OWER = 0x3CF;
+    // Set pins to outputs
+    pinMode(11, OUTPUT);
+    pinMode(12, OUTPUT);
+    for(i = 25; i < 31; i++) {
+      pinMode(i, OUTPUT);
+    }
+    
+    for(addr = 0; addr < NUM_ADDRESSES; addr++) { //write loop
         
-        ++addressSteps;
+      writeData(addressInt, correctData);
+       
+      // Strange addition is necessary to ensure that dedicated pins do not get set. In this case, pins 33 through 41 are being used and then pins 45 through 51. Thus, when the following situation is met:
+      // 0000000 0000 111111111 0 -> 0000001 0000 000000000 0 
+      if ((addressInt & ADDRESS_MASK) < (ADDRESS_MASK - 1)) {
+         addressInt = addressInt + 2;
       }
       else {
-      
-        if(addressSteps == 0) { // Reset all inputs to outputs
-          pinMode(11, INPUT);
-          pinMode(12, INPUT);
-          for(i = 25; i < 31; i++) {
-            pinMode(i, INPUT);
-          }
-        }
-        // Used to see if output is consistent with initial input
-        dataInt = readData(addressInt);
-        
-        // Print out the address and received data if bad data read
-        if(!check(correctData, dataInt)) {
-          
-          if (VERBOSE){
-            Serial.print("Address:\t");
-            Serial.println(addressSteps);
-            Serial.print("Correct data:\t");
-            Serial.println(correctData);
-            Serial.print("Read data:\t");
-            Serial.println(dataInt);
-            Serial.print("XOR of the data:\t");
-            Serial.println(toBinary(dataInt ^ correctData, 10));
-            Serial.println();
-          }
-          else {
-            Serial.println(addressSteps);
-            Serial.println(correctData);
-            Serial.println(dataInt);
-            Serial.println();
-          }
-
-          // If the read-back data is incorrect reread n times, n = readsAfterFailure
-          reread(addressSteps, readsAfterFailure);
-        }
-        
-        //increment the address
-        addressSteps = addressSteps + 1;
-        if((addressInt & ADDRESS_MASK) < (ADDRESS_MASK - 1)) {
-          addressInt = addressInt + 2;
-        }
-        else {
-          addressInt = addressInt + 3074;
-        }
+        addressInt = addressInt + 3074;
       }
     }
     
-    // Way to tell if finished running through addresses
-    else if(addressSteps == NUM_ADDRESSES) {
-      if(writing) {
-        writing = false;
-        addressSteps = 0;
-        addressInt = 0;
-        delay(delayAfterWrite);
-      }
-      else {
+    // Set pins to inputs
+    pinMode(11, INPUT);
+    pinMode(12, INPUT);
+    for(i = 25; i < 31; i++) {
+      pinMode(i, INPUT);
+    }
+    
+    for(addr = 0; addr < NUM_ADDRESSES; addr++) { //read loop
+
+      // Used to see if output is consistent with initial input
+      dataInt = readData(addressInt);
+      
+      // Print out the address and received data if bad data read
+      if(!check(correctData, dataInt)) {
         
-        if (VERBOSE) {
-          Serial.println("Done with current run\n");
+        if (VERBOSE){
+          Serial.print("Address:\t");
+          Serial.println(addr);
+          Serial.print("Correct data:\t");
+          Serial.println(correctData);
+          Serial.print("Read data:\t");
+          Serial.println(dataInt);
+          Serial.print("XOR of the data:\t");
+          Serial.println(toBinary(dataInt ^ correctData, 10));
         }
         else {
-          Serial.println("--\n");
+          Serial.println(addr);
+          Serial.println(correctData);
+          Serial.println(dataInt);
         }
+
+          // If the read-back data is incorrect reread n times, n = readsAfterFailure
+          reread(addr, readsAfterFailure);
+      }
         
-        currentRun = currentRun + 1;
-        writing = true;
-        addressInt = 0;
-        addressSteps = 0;
+      //increment the address
+      if((addressInt & ADDRESS_MASK) < (ADDRESS_MASK - 1)) {
+        addressInt = addressInt + 2;
+      }
+      else {
+        addressInt = addressInt + 3074;
       }
     }
+    
+    if (VERBOSE) {
+      Serial.println("Done with current run\n");
+    }
+    else {
+      Serial.println("--\n");
+    }
+    addressInt = 0;
+    addr = 0;
   }
+  
+  if (VERBOSE) {
+    Serial.println("Done with all runs, ready for more data\n");
+  }
+  else {
+    Serial.println("----\n");
+  }
+  
 }
 
 
@@ -198,20 +170,19 @@ bool check(const int& oldData, const int& data) {
 
 
 void getData() {
+  int mod = 0;
   bool done = false;
   while(!done) {
-    divider = (divider + 1) % 100000;
-    if(divider < 50000) {
-       digitalWrite(LED, HIGH);
-    }
-    else {
-       digitalWrite(LED, LOW);
-    }
-
     if (Serial.available()) {
       done = processIncomingByte (Serial.read());
     }
   }
+  
+  // Using PD0 through PD9 without PD4 and PD5. Thus, need to split the data at PD4 and PD5; 
+  mod = correctData & 15; // Get the last 4 digits
+  correctData >>= 4;  // Bit shift up to where 0's should start
+  correctData <<= 6;  // Add the two extra zeros
+  correctData += mod; // Re-add mod
 }
 
 
@@ -315,7 +286,7 @@ int readData(const int& address) {
  
  
 // Write a byte of data
-void writeData(const int& address,const int& data) {
+void writeData(const int& address, const int& data) {
   REG_PIOC_ODSR = address;
   REG_PIOD_ODSR = data;
   
